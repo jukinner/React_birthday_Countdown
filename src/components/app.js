f25926aa4c335f577c6f8ebdf5c9aea5c71a3162
Justin Kinner <jkinner@gmail.com>
2018-08-09 08:52:30 -0600

    updated package.json to include datepicker and set up Birthdayform.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import BirthdayForm from './birthdayForm.js';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>DevCamp React Starter</h1>
+      	Birthday Form
       </div>
     );
   }
---
+++ src/components/birthdayForm.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+
+class BirthdayForm extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+
+	render() {
+		return (
+				<div>
+					<h1>Birthday Form Component</h1>
+					<DatePicker>
+				</div>
+			)
+
+	}
+}
+
+export default BirthdayForm;
