c43094649b365855aa9f2c8986a5d43a28431bc3
Jukinner <jukinner@gmail.com>
2018-08-14 08:00:38 -0600

    added clock component calculation to calculate difference in time

--- src/components/clock.js
+++ src/components/clock.js
@@ -5,8 +5,29 @@ class Clock extends Component {
   constructor(props) {
     super(props)
 
+    this.getTimeRemaining = this.getTimeRemaining.bind(this);
+
     this.state = {
-      timeRemaining: {}
+      timeRemaining: this.getTimeRemaining(this.props.birthdayFormState.startDate.toString())
+    }
+  }
+
+  getTimeRemaining(birthday) {
+    var bday = new Date(birthday);
+    let today = new Date();
+
+    var distance = bday.getTime() - today.getTime();
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    return {
+      'days': days,
+      'hours': hours, 
+      'minutes': minutes,
+      'seconds': seconds
     }
   }
   
@@ -24,10 +45,6 @@ class Clock extends Component {
       )
   }
 
-  componentDidMount() {
-    this.birthday = this.props.birthdayFormState.startDate.toString();
-    alert(this.birthday);
-  }
 }
 
 
