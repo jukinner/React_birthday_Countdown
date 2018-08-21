578f828bfad242450dcad94078c1b05e7f14810a
Jukinner <jukinner@gmail.com>
2018-08-20 20:49:59 -0600

    calculated conditions and logic for countdown timer

--- src/components/clock.js
+++ src/components/clock.js
@@ -18,6 +18,31 @@ class Clock extends Component {
     var bday = new Date(birthday);
     let today = new Date();
 
+    const currentMonth = today.getMonth();
+    const birthMonth = bday.getMonth();
+    
+    if(birthMonth > currentMonth){
+    //1 month is after the current month
+    bday.setFullYear(today.getFullYear());
+    } 
+    else if (birthMonth < currentMonth) {
+    //2. month is b4 the current month
+    bday.setFullYear(today.getFullYear() + 1);
+    } 
+    else if (birthMonth == currentMonth) {
+      const birthDay = bday.getDate();
+      const currentDay = today.getDate();
+      if(birthDay > currentDay){
+      //1 day is after the current day
+      bday.setFullYear(today.getFullYear());
+      } 
+      else if (birthDay < currentDay) {
+      //2. day is b4 the current day
+      bday.setFullYear(today.getFullYear() + 1);
+    }
+  }
+
+
     var distance = bday.getTime() - today.getTime();
 
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
