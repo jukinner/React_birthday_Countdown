9ddaa172aad67f5b709e614333a28d29c41ff1d1
Jukinner <jukinner@gmail.com>
2018-08-23 21:11:15 -0600

    added styles to clock and change date

--- src/components/birthdayForm.js
+++ src/components/birthdayForm.js
@@ -43,7 +43,7 @@ class BirthdayForm extends Component {
 					this.state.formCompleted ? 
 					<div>
 						<Clock birthdayFormState={this.state}/>
-						<a onClick={this.handleChangeDate}>Change Date</a>
+						<a className="change-date" onClick={this.handleChangeDate}>Change Date</a>
 					</div>
 					:
 					<div>
